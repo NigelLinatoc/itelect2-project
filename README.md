dc413f122f7869640b3b925ea7b562008e0d8754
Nigel Andrei D. Linatoc <nigelandreilinatoc@gmail.com>
2026-08-11 23:40:35 +0800

    GT6: Complete CRUD REST API with middleware

--- README.md
+++ README.md
@@ -2,3 +2,23 @@
 My IT Elective 2 backend web development project.
 
 by: Nigel Andrei D. Linatoc - for the first semester of SY 2026-2027.
+
+
+## API Testing
+
+**1. GET**
+![alt text](screenshots/get.png)
+
+**2. POST**
+![alt text](screenshots/post.png)
+![alt text](screenshots/post2.png)
+![alt text](screenshots/post_error_400.png)
+
+**3. PUT**
+![alt text](screenshots/put.png)
+![alt text](screenshots/put_error_404.png)
+
+**4. DELETE**
+![alt text](screenshots/delete.png)
+![alt text](screenshots/delete2.png)
+![alt text](screenshots/delete_error_404.png)
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {tasks} from "../src/utils.js";
 import {fetchSampleUsers} from "../src/api.js";
-
+import {mergeTaskUpdate, validateTask} from "../src/utils.js";
 const router = express.Router();
 
 router.get("/tasks", (req, res) => {
@@ -24,6 +24,36 @@ router.get("/tasks/:id", (req, res) => {
 router.get("/users", async (req, res) => {
     const users = await fetchSampleUsers();
     res.status(200).json(users);
+});
+
+let nextId = 4;
+router.post("/tasks", (req, res) => {
+if (!validateTask(req.body)) {
+return res.status(400).json({ error: "title and due date required" });
+}
+const task = { id: nextId++, ...req.body };
+tasks.push(task);
+res.status(201).json(task);
+});
+
+router.put("/tasks/:id", (req, res) => {
+const id = Number(req.params.id);
+const index = tasks.findIndex((t) => t.id === id);
+if (index === -1) {
+return res.status(404).json({ error: "Task not found" });
+}
+tasks[index] = mergeTaskUpdate(tasks[index], req.body);
+res.status(200).json(tasks[index]);
+});
+
+router.delete("/tasks/:id", (req, res) => {
+const id = Number(req.params.id);
+const index = tasks.findIndex((t) => t.id === id);
+if (index === -1) {
+return res.status(404).json({ error: "Task not found" });
+}
+const [removed] = tasks.splice(index, 1);
+res.status(200).json({ message: "Deleted", task: removed });
 });
 
 export default router;
